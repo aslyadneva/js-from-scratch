77831d4b41bef184bb01421ec2eb29f0213f6891
Anastasia <slyadneva.anastasia@gmail.com>
2025-10-17 09:19:06 -0500

    layout fix

--- src/blocks/BankAccount.tsx
+++ src/blocks/BankAccount.tsx
@@ -12,7 +12,7 @@ function BankAccount({
   const color = type === "checking" ? "chart-1" : "chart-4";
   //   const color = "chart-3";
   return (
-    <li className="flex items-start gap-3 py-4">
+    <li className="flex flex-col items-start gap-3 py-4 md:flex-row">
       <span
         className={`focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-${color} inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden rounded-md border border-transparent p-1 text-xs font-medium whitespace-nowrap text-white transition-[color,box-shadow] focus-visible:ring-[3px] [&>svg]:pointer-events-none [&>svg]:size-3`}
       >
